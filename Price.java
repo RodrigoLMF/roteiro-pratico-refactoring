e9ae3975b5d3e0eba4bd94db51da590465259f2f
Rodrigo Ferreira <digo@fedora.(none)>
2022-06-25 23:46:42 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -2,16 +2,7 @@ public abstract class Price {
    public abstract int getPriceCode();
   public abstract double getCharge(int daysRented);
 
-  public int getFrequentRenterPoints(int daysRented) {
-      //Adicionar o trecho de código extraído.
-       //Adicionar o trecho de código extraído.
-    //Adicionar o trecho de código extraído.
-    int frequentRenterPoints = 0;
-    // add frequent renter points
-    frequentRenterPoints ++;
-    // add bonus for a two day new release rental
-    if ((getPriceCode() == Movie.NEW_RELEASE) &&
-       daysRented > 1) frequentRenterPoints ++;
-    return frequentRenterPoints;
+    public int getFrequentRenterPoints(int daysRented) {
+       return 1;
    }
 } 
