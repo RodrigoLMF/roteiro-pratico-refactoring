6eceb63a068e68f23f7b2c82f2c52c53410da3c3
Rodrigo Ferreira <digo@fedora.(none)>
2022-06-25 23:59:04 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,35 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
-      }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
-      return result;
-   }
-} 
+      public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeaderText(aCustomer.getName());
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for this rental
+            result += getRentalText(each);
+        }
+        // add footer lines
+        result += getFooterText(aCustomer);
+        return result;
+    }
+
+    private String getHeaderText(String customerName) {
+        return "Rental Record for " + customerName + "\n";
+    }
+
+    private String getRentalText(Rental rental) {
+        return "\t" + rental.getMovie().getTitle() + "\t" +
+                String.valueOf(rental.getCharge()) + "\n";
+    }
+
+    private String getFooterText(Customer customer) {
+        String result = "";
+        result += "Amount owed is " +
+                String.valueOf(customer.getTotalCharge()) + "\n";
+        result += "You earned " +
+                String.valueOf(customer.getTotalFrequentRenterPoints()) +
+                " frequent renter points";
+        return result;
+    }
+}	
